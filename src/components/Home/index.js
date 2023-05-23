8304f50b88efb147b08c6c2d3951ca9500f4bf6c
Muhammad Ahmed <muhammednch@gmail.com>
2023-05-22 20:29:55 -0400

    added bouncing letters

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -11,10 +11,10 @@ const Home = () => {
 
 
     useEffect(() => {
-    return setTimeout(() => {
-        setLetterClass('text-animate-hover')
-    }, 4000)
-}, [])
+        return setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 4000)
+      }, [])
 
 
 
